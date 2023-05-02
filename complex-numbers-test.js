ac92b168db5b630faff1c457d37856f76478778a
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-05-02 19:44:07 +0530

    Added more test Cases for Get real part and get imaginary part

--- complex-numbers-test.js
+++ complex-numbers-test.js
@@ -6,19 +6,38 @@ const { complexNumber } = require('./complex-numbers.js');
 let actual = "";
 let expected = "";
 
-describe("Testing for complex number 2 + 3i", function() {
+describe("Testing for get real Part", function() {
   const complexNum = complexNumber(); 
 
-  it("Testing for get real Part", function() {
-    actual = complexNum.getRealPart("2 + 3i");
-    expected = 2; 
+  it("Should give zero when the real part is 0, 0 + 2i", function() {
+    actual = complexNum.getRealPart("0 + 2i");
+    expected = 0; 
     deepStrictEqual(actual, expected);
   });
 
-  it("Testing for get imaginary Part", function() {
+  it("Should give the number when real part is negative, 2 + 3i", function() {
+
+    actual = complexNum.getRealPart("2 + 3i");
+    expected = 2;
+    deepStrictEqual(actual, expected);
+  });
+});
+
+
+describe("Testing for get imaginary Part", function() {
+  const complexNum = complexNumber(); 
+
+  it("Should give zero when the imaginary part is 0i", function() {
+    actual = complexNum.getImaginaryPart("2 + 0i");
+    expected = 0;
+    deepStrictEqual(actual, expected);
+  });
+
+  it("Should give 3 when  the imaginary part is 3i", function() {
     actual = complexNum.getImaginaryPart("2 + 3i");
     expected = 3;
     deepStrictEqual(actual, expected);
   });
+
 });
 
