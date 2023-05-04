5a99363e88c22cb84171527870d96113574cc6fc
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-05-04 15:17:10 +0530

    Changed Approach to use bind

--- src/complex-numbers.js
+++ src/complex-numbers.js
@@ -2,46 +2,57 @@ const getSign = function(number) {
   return number < 0 ? "-" : "+";
 }
 
+const getRealPart = function() {
+  return this.real;
+}
+
+const getImaginaryPart = function() {
+  return this.imaginary;
+}
+
+const add = function(augend) {
+  const sumOfReal = this.getRealPart() + augend.getRealPart();
+  const sumOfImaginary = this.getImaginaryPart() + augend.getImaginaryPart();
+
+  return complexNum(sumOfReal, sumOfImaginary);
+}
+
+const multiply = function(multiplier) {
+  const a = this.getRealPart();
+  const b = this.getImaginaryPart();
+  const c = multiplier.getRealPart();
+  const d = multiplier.getImaginaryPart();
+
+  const productOfReal = ( a * c ) - ( b * d );
+  const productOfImaginary = ( a * d ) + ( b * c );
+
+  return complexNum(productOfReal, productOfImaginary);
+}
+
+const toString = function() {
+  const imaginary = this.getImaginaryPart();
+  const real = this.getRealPart();
+  const sign = getSign(imaginary);
+  const imaginaryAbs = Math.abs(imaginary);
+
+  if(imaginary === 0) return `${real}`;
+  if(real === 0) return `${imaginary}i`;
+
+  return `${real} ${sign} ${imaginaryAbs}i`;
+}
+
+
 const complexNum = function(real, imaginary) {
+  const parts = {real, imaginary}
+  const complexNumber = {};
 
-  const getRealPart = function() {
-    return real;
-  }
+  complexNumber.getRealPart = getRealPart.bind(parts);
+  complexNumber.getImaginaryPart = getImaginaryPart.bind(parts);
+  complexNumber.toString = toString.bind(complexNumber);
+  complexNumber.add = add.bind(complexNumber);
+  complexNumber.multiply = multiply.bind(complexNumber);
 
-  const getImaginaryPart = function() {
-    return imaginary;
-  }
-
-  const add = function(augend) {
-    const sumOfReal = real + augend.getRealPart();
-    const sumOfImaginary = imaginary + augend.getImaginaryPart();
-
-    return complexNum(sumOfReal, sumOfImaginary);
-  }
-
-  const multiply = function(multiplier) {
-    const a = real;
-    const b = imaginary;
-    const c = multiplier.getRealPart();
-    const d = multiplier.getImaginaryPart();
-
-    const productOfReal = ( a * c ) - ( b * d );
-    const productOfImaginary = ( a * d ) + ( b * c );
-
-    return complexNum(productOfReal, productOfImaginary);
-  }
-
-  const toString = function() {
-    const sign = getSign(imaginary);
-    const imaginaryAbs = Math.abs(imaginary);
-
-    if(imaginary === 0) return `${real}`;
-    if(real === 0) return `${imaginary}i`;
-
-    return `${real} ${sign} ${imaginaryAbs}i`;
-  }
-
-  return { getRealPart, getImaginaryPart, add, multiply, toString };
+  return complexNumber;
 }
 
 exports.complexNum = complexNum;
